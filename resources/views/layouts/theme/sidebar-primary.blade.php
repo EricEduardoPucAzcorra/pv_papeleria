f94050ad5f05151d77890a7a4d88487c98990ce8
licsican <licsican@gmail.com>
2023-05-28 01:03:05 -0600

    Cambios de en el menu

--- resources/views/layouts/theme/sidebar-primary.blade.php
+++ resources/views/layouts/theme/sidebar-primary.blade.php
@@ -29,7 +29,7 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-               <li class="nav-header">PRINCIPAL</li>
+               <li class="nav-header" style="background-color: #ffffff; color: rgb(8, 8, 8)">PRINCIPAL</li>
                 <li class="nav-item">
                     <a href="{{route('dashboard')}}" class="nav-link {{!Route::is('dashboard') ?: 'active' }}">
                         <i class="fas fa-tachometer-alt"></i>
@@ -59,7 +59,7 @@
                     </a>
                 </li>
 
-                <li class="nav-header">INVENTARIO</li>
+                <li class="nav-header" style="background-color: #ffffff; color: rgb(8, 8, 8)">INVENTARIO</li>
 
                 <li class="nav-item">
                     <a href="{{route('categorias')}}" class="nav-link {{!Route::is('categorias') ?: 'active' }}">
@@ -80,7 +80,7 @@
                     </a>
                 </li>
 
-                <li class="nav-header">REPORTES</li>
+                <li class="nav-header" style="background-color: #ffffff; color: rgb(8, 8, 8)">REPORTES</li>
 
                 <li class="nav-item">
                     <a href="{{route('deudas.index')}}" class="nav-link {{!Route::is('deudas.*') ?: 'active'}}">
@@ -107,7 +107,7 @@
                     </a>
                 </li>
 
-                <li class="nav-header">CUENTA</li>
+                <li class="nav-header" style="background-color: #ffffff; color: rgb(8, 8, 8)">CUENTA</li>
 
                 {{-- <li class="nav-item">
                     <a href="#" class="nav-link">
